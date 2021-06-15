f1be57c063e052ec859b4a4da11f1eec98a810c0
Jay Jenks <jamesljenks@gmail.com>
2021-06-15 01:15:06 -0400

    Restructured directories to incorporate routes folder (containing apiRoutes.js and htmlRoutes.js) and added data folder.

---
+++ server.js
@@ -0,0 +1,87 @@
+// DEPENDENCIES
+// Series of npm packages that we will use to give our server useful functionality
+
+const fs = require('fs'); // DO I NEED THIS?
+const express = require('express');
+
+// EXPRESS CONFIGURATION
+// This sets up the basic properties for our express server
+
+// Tells node that we are creating an "express" server
+const app = express();
+
+// Sets an initial port. We"ll use this later in our listener
+const PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Sets up the Express app to serve static assets directly
+app.use(express.static('public'));
+
+
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
+
+
+// LISTENER
+// The below code effectively "starts" our server
+
+app.listen(PORT, () => {
+    console.log(`App listening on PORT: ${PORT}`);
+  });
+
+
+
+// END server.js HERE
+
+
+
+
+function createNewNote(body, notesArray) {
+    const newNote = body;
+    if (!Array.isArray(notesArray))
+    notesArray = [];
+
+    if (notesArray.length === 0)
+    notesArray.push(0);
+
+    body.id = notesArray[0];
+    notesArray[0]++;
+
+    notesArray.push(newNote);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db/json'),
+        JSON.stringify(notesArray, null, 2)
+    );
+    return newNote;
+}
+
+app.post('/api/notes', (req, res) => {
+    const newNote = createNewNote(req.body, allNotes);
+    res.json(newNote);
+});
+
+function deleteNote(id,notesArray) {
+    for (let i = 0; i < notesArray.length; i++) {
+        let note = notesArray[i];
+
+        if (note.id == id) {
+            notesArray.splice(i, 1);
+            fs.writeFileSync(
+                path.join(__dirname, './db/db/json'),
+                JSON.stringify(notesArray, null, 2)
+            );
+            break;
+        }
+    }
+}
+
+app.delete('/api/notes/:id', (req, res) => {
+    
+})
